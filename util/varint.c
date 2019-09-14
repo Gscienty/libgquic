538ec945d4d25dc085b93293e90a8912d425284e
Gscienty <gaoxiaochuan@hotmail.com>
2019-09-13 23:17:16 -0400

    fix: varint deserialize (sign bits)

--- util/varint.c
+++ util/varint.c
@@ -6,19 +6,19 @@ int gquic_varint_wrap(gquic_util_varint_t *varint, const unsigned long value) {
     }
     varint->length = 0;
     varint->value = 0;
-    if (0x3F >= value) {
+    if (0x3f >= value) {
         varint->length = 1;
         varint->value = value;
     }
-    else if (0x3FFF >= value) {
+    else if (0x3fff >= value) {
         varint->length = 2;
         varint->value = value;
     }
-    else if (0x3FFFFFFF >= value) {
+    else if (0x3fffffff >= value) {
         varint->length = 4;
         varint->value = value;
     }
-    else if (0x3FFFFFFFFFFFFFFF >= value) {
+    else if (0x3fffffffffffffff >= value) {
         varint->length = 8;
         varint->value = value;
     }
@@ -82,41 +82,41 @@ ssize_t gquic_varint_deserialize(gquic_util_varint_t *varint, const void *buf, c
     }
     varint->length = 0;
     varint->value = 0;
-    switch (((unsigned char *) buf)[0] & 0xC0) {
+    switch (((unsigned char *) buf)[0] & 0xc0) {
     case 0x00:
         varint->length = 1;
         if (varint->length > size) {
             return -3;
         }
-        ((unsigned char *) &varint->value)[0] = ((unsigned char *) buf)[0] & 0x3F;
+        ((unsigned char *) &varint->value)[0] = ((unsigned char *) buf)[0] & 0x3f;
         break;
 
-    case 0x10:
+    case 0x40:
         varint->length = 2;
         if (varint->length > size) {
             return -3;
         }
-        ((unsigned char *) &varint->value)[1] = ((unsigned char *) buf)[0] & 0x3F;
+        ((unsigned char *) &varint->value)[1] = ((unsigned char *) buf)[0] & 0x3f;
         ((unsigned char *) &varint->value)[0] = ((unsigned char *) buf)[1];
         break;
 
-    case 0x20:
+    case 0x80:
         varint->length = 4;
         if (varint->length > size) {
             return -3;
         }
-        ((unsigned char *) &varint->value)[3] = ((unsigned char *) buf)[0] & 0x3F;
+        ((unsigned char *) &varint->value)[3] = ((unsigned char *) buf)[0] & 0x3f;
         ((unsigned char *) &varint->value)[2] = ((unsigned char *) buf)[1];
         ((unsigned char *) &varint->value)[1] = ((unsigned char *) buf)[2];
         ((unsigned char *) &varint->value)[0] = ((unsigned char *) buf)[3];
         break;
 
-    case 0x30:
+    case 0xc0:
         varint->length = 8;
         if (varint->length > size) {
             return -3;
         }
-        ((unsigned char *) &varint->value)[7] = ((unsigned char *) buf)[0] & 0x3F;
+        ((unsigned char *) &varint->value)[7] = ((unsigned char *) buf)[0] & 0x3f;
         ((unsigned char *) &varint->value)[6] = ((unsigned char *) buf)[1];
         ((unsigned char *) &varint->value)[5] = ((unsigned char *) buf)[2];
         ((unsigned char *) &varint->value)[4] = ((unsigned char *) buf)[3];
