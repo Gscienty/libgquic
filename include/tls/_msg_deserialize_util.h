1431c48e9e5673ea7dc6f05846b4c97b3adfc811
Gscienty <gaoxiaochuan@hotmail.com>
2019-09-27 05:40:16 -0400

    feat: add inline (ser/deser) methods

---
+++ include/tls/_msg_deserialize_util.h
@@ -0,0 +1,32 @@
+#include "util/big_endian.h"
+#include "util/str.h"
+#include <unistd.h>
+#include <string.h>
+
+static inline int __gquic_recovery_bytes(void *, const size_t, const void *, const size_t, size_t *);
+static inline int __gquic_recovery_str(gquic_str_t *, const size_t, const void *, const size_t, size_t *);
+
+static inline int __gquic_recovery_bytes(void *ret, const size_t bytes, const void *buf, const size_t size, size_t *off) {
+    if (bytes > size - *off) {
+        return -1;
+    }
+    gquic_big_endian_transfer(ret, buf + *off, bytes);
+    *off += bytes;
+    return 0;
+}
+
+static inline int __gquic_recovery_str(gquic_str_t *str, const size_t bytes, const void *buf, const size_t size, size_t *off) {
+    if (str == NULL || buf == NULL) {
+        return -1;
+    }
+    if (bytes > size - *off) {
+        return -2;
+    }
+    if (gquic_str_alloc(str, bytes) != 0) {
+        return -3;
+    }
+    memcpy(str->val, buf + *off, bytes);
+    *off += bytes;
+
+    return 0;
+}
---
+++ include/tls/_msg_serialize_util.h
@@ -0,0 +1,56 @@
+#include "util/big_endian.h"
+#include "util/str.h"
+#include "util/list.h"
+#include <sys/types.h>
+#include <string.h>
+
+static inline void __gquic_stack_push(gquic_list_t *, const size_t);
+static inline size_t __gquic_stack_pop(gquic_list_t *);
+static inline void __gquic_fill_prefix_len(gquic_list_t *, void *, const size_t, const size_t);
+static inline void __gquic_store_prefix_len(gquic_list_t *, size_t *, const size_t);
+static inline void __gquic_fill_4byte(void *, size_t *, const u_int32_t);
+static inline void __gquic_fill_2byte(void *, size_t *, const u_int16_t);
+static inline void __gquic_fill_1byte(void *, size_t *, const u_int8_t);
+static inline void __gquic_fill_str(void *, size_t *, const gquic_str_t *);
+
+static inline void __gquic_stack_push(gquic_list_t *stack, const size_t val) {
+    gquic_list_insert_after(stack, gquic_list_alloc(sizeof(size_t)));
+    *(size_t *) gquic_list_next(GQUIC_LIST_PAYLOAD(stack)) = val;
+}
+
+static inline size_t __gquic_stack_pop(gquic_list_t *stack) {
+    size_t ret = *(size_t *) gquic_list_next(GQUIC_LIST_PAYLOAD(stack));
+    gquic_list_release(gquic_list_next(GQUIC_LIST_PAYLOAD(stack)));
+    return ret;
+}
+
+static inline void __gquic_fill_prefix_len(gquic_list_t *stack, void *buf, const size_t off, const size_t len) {
+    size_t prefix_len_off = __gquic_stack_pop(stack);
+    size_t prefix_len = off - len - prefix_len_off;
+    gquic_big_endian_transfer(buf + prefix_len_off, &prefix_len, len);
+}
+
+static inline void __gquic_store_prefix_len(gquic_list_t *stack, size_t *off, const size_t len) {
+    __gquic_stack_push(stack, *off);
+    *off += len;
+}
+
+static inline void __gquic_fill_4byte(void *buf, size_t *off, const u_int32_t val) {
+    gquic_big_endian_transfer(buf + *off, &val, 4);
+    *off += 4;
+}
+
+static inline void __gquic_fill_2byte(void *buf, size_t *off, const u_int16_t val) {
+    gquic_big_endian_transfer(buf + *off, &val, 2);
+    *off += 2;
+}
+
+static inline void __gquic_fill_1byte(void *buf, size_t *off, const u_int8_t val) {
+    gquic_big_endian_transfer(buf + *off, &val, 1);
+    *off += 1;
+}
+
+static inline void __gquic_fill_str(void *buf, size_t *off, const gquic_str_t *str) {
+    memcpy(buf + *off, str->val, str->size);
+    *off += str->size;
+}
