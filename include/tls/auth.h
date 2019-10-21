5325b9146917397b81021031ce62665f34938d94
Gscienty <gaoxiaochuan@hotmail.com>
2019-10-21 01:08:18 -0400

    feat: add verify handshake sign

--- include/tls/auth.h
+++ include/tls/auth.h
@@ -2,6 +2,7 @@
 #define _LIBGQUIC_TLS_AUTH_H
 
 #include "util/list.h"
+#include "util/str.h"
 #include <openssl/evp.h>
 
 int gquic_tls_selected_sigalg(u_int16_t *const sigalg,
@@ -11,5 +12,11 @@ int gquic_tls_selected_sigalg(u_int16_t *const sigalg,
                               const gquic_list_t *const peer_sigalgs,
                               const gquic_list_t *const self_sigalgs,
                               const u_int16_t tls_ver);
+
+int gquic_tls_verify_handshake_sign(const u_int8_t sig_type,
+                                    const EVP_MD *const hash,
+                                    const EVP_PKEY *const pubkey,
+                                    const gquic_str_t *sign,
+                                    const gquic_str_t *sig);
 
 #endif
--- tls/auth.c
+++ tls/auth.c
@@ -6,15 +6,15 @@
 int gquic_tls_selected_sigalg(u_int16_t *const sigalg,
                               u_int8_t *const sig_type,
                               const EVP_MD **const hash,
-                              const EVP_PKEY *const pkey,
+                              const EVP_PKEY *const pubkey,
                               const gquic_list_t *const peer_sigalgs,
                               const gquic_list_t *const self_sigalgs,
                               const u_int16_t tls_ver) {
     int pkey_id;
-    if (sigalg == NULL || sig_type == NULL || hash == NULL || pkey == NULL || peer_sigalgs == NULL || self_sigalgs == NULL || tls_ver == 0) {
+    if (sigalg == NULL || sig_type == NULL || hash == NULL || pubkey == NULL || peer_sigalgs == NULL || self_sigalgs == NULL || tls_ver == 0) {
         return -1;
     }
-    pkey_id = EVP_PKEY_id(pkey);
+    pkey_id = EVP_PKEY_id(pubkey);
     if (tls_ver < GQUIC_TLS_VERSION_12 || gquic_list_head_empty(peer_sigalgs)) {
         switch (pkey_id) {
         case EVP_PKEY_RSA:
@@ -83,4 +83,45 @@ int gquic_tls_selected_sigalg(u_int16_t *const sigalg,
     }
 
     return -7;
+}
+
+int gquic_tls_verify_handshake_sign(const u_int8_t sig_type,
+                                    const EVP_MD *const hash,
+                                    const EVP_PKEY *const pubkey,
+                                    const gquic_str_t *sign,
+                                    const gquic_str_t *sig) {
+    (void) sig_type;
+    EVP_MD_CTX *ctx;
+    int pubkey_type = 0;
+    int ret = 0;
+    if (pubkey == NULL || sign == NULL || sig == NULL) {
+        return -1;
+    }
+    switch (sig_type) {
+    case GQUIC_SIG_ECDSA:
+        pubkey_type = EVP_PKEY_EC;
+        break;
+    case GQUIC_SIG_ED25519:
+        pubkey_type = EVP_PKEY_ED25519;
+        break;
+    case GQUIC_SIG_PKCS1V15:
+    case GQUIC_SIG_RSAPSS:
+        pubkey_type = EVP_PKEY_RSA;
+        break;
+    default:
+        return -2;
+    }
+    ctx = EVP_MD_CTX_new();
+    if (EVP_DigestVerifyInit(ctx, NULL, hash, NULL, (EVP_PKEY *const) pubkey) <= 0) {
+        goto failure;
+    }
+    if ((ret = EVP_DigestVerify(ctx, GQUIC_STR_VAL(sig), GQUIC_STR_SIZE(sig), GQUIC_STR_VAL(sign), GQUIC_STR_SIZE(sign))) != 1) {
+        goto failure;
+    }
+
+    EVP_MD_CTX_free(ctx);
+    return 0;
+failure:
+    EVP_MD_CTX_free(ctx);
+    return -3;
 }
