e2978c82055a18f4e56a70df291c7b7c7624438e
Gscienty <gaoxiaochuan@hotmail.com>
2019-12-01 21:27:35 -0500

    refactor: samplify varint

--- include/util/varint.h
+++ include/util/varint.h
@@ -1,18 +1,10 @@
 #ifndef _LIBGQUIC_UTIL_VARINT_H
 #define _LIBGQUIC_UTIL_VARINT_H
 
-#include <unistd.h>
+#include <sys/types.h>
 
-typedef struct gquic_varint_s gquic_varint_t;
-struct gquic_varint_s {
-    unsigned char length;
-    unsigned long value;
-};
-
-int gquic_varint_wrap(gquic_varint_t *, const unsigned long);
-
-ssize_t gquic_varint_serialize(const gquic_varint_t *, void *, const size_t);
-
-ssize_t gquic_varint_deserialize(gquic_varint_t *, const void *, const size_t);
+ssize_t gquic_varint_size(const u_int64_t *const val);
+ssize_t gquic_varint_serialize(const u_int64_t *const val, void *const buf, const size_t size);
+ssize_t gquic_varint_deserialize(u_int64_t *const val, const void *const buf, const size_t size);
 
 #endif
--- util/varint.c
+++ util/varint.c
@@ -1,121 +1,104 @@
 #include "util/varint.h"
 #include "util/big_endian.h"
 
-int gquic_varint_wrap(gquic_varint_t *varint, const unsigned long value) {
-    if (varint == NULL) {
+ssize_t gquic_varint_size(const u_int64_t *const val) {
+    if (val == NULL) {
         return -1;
     }
-    varint->length = 0;
-    varint->value = 0;
-    if (0x3f >= value) {
-        varint->length = 1;
-        varint->value = value;
+    if (0x3f >= *val) {
+        return 1;
     }
-    else if (0x3fff >= value) {
-        varint->length = 2;
-        varint->value = value;
+    else if (0x3fff >= *val) {
+        return 2;
     }
-    else if (0x3fffffff >= value) {
-        varint->length = 4;
-        varint->value = value;
+    else if (0x3fffffff >= *val) {
+        return 4;
     }
-    else if (0x3fffffffffffffff >= value) {
-        varint->length = 8;
-        varint->value = value;
+    else if (0x3fffffffffffffff >= *val) {
+        return 8;
     }
     else {
-        return -1;
+        return -2;
     }
     return 0;
 }
 
-ssize_t gquic_varint_serialize(const gquic_varint_t *varint, void *buf, const size_t size) {
-    if (varint == NULL) {
+ssize_t gquic_varint_serialize(const u_int64_t *const val, void *const buf, const size_t size) {
+    size_t length = 0;
+    if (val == NULL || buf == NULL) {
         return -1;
     }
-    if (varint->length > size) {
+    length = gquic_varint_size(val);
+    if (length > size) {
         return -2;
     }
-    if (buf == NULL) {
-        return -3;
-    }
-    switch (varint->length) {
+    switch (length) {
     case 1:
-        gquic_big_endian_transfer(buf, &varint->value, 1);
+        gquic_big_endian_transfer(buf, val, 1);
         break;
 
     case 2:
-        gquic_big_endian_transfer(buf, &varint->value, 2);
+        gquic_big_endian_transfer(buf, val, 2);
         ((unsigned char *) buf)[0] |= 0x40;
         break;
 
     case 4:
-        gquic_big_endian_transfer(buf, &varint->value, 4);
+        gquic_big_endian_transfer(buf, val, 4);
         ((unsigned char *) buf)[0] |= 0x80;
         break;
 
     case 8:
-        gquic_big_endian_transfer(buf, &varint->value, 8);
+        gquic_big_endian_transfer(buf, val, 8);
         ((unsigned char *) buf)[0] |= 0xc0;
         break;
 
     default:
-        return -4;
+        return -3;
     }
 
-    return varint->length;
+    return length;
 }
 
-ssize_t gquic_varint_deserialize(gquic_varint_t *varint, const void *buf, const size_t size) {
-    if (varint == NULL) {
+ssize_t gquic_varint_deserialize(u_int64_t *const val, const void *const buf, const size_t size) {
+    if (val == NULL || buf == NULL) {
         return -1;
     }
-    if (buf == NULL) {
-        return -2;
-    }
-    varint->length = 0;
-    varint->value = 0;
+    *val = 0;
     switch (((unsigned char *) buf)[0] & 0xc0) {
     case 0x00:
-        varint->length = 1;
-        if (varint->length > size) {
+        if (1 > size) {
             return -3;
         }
-        gquic_big_endian_transfer(&varint->value, buf, 1);
-        ((unsigned char *) &varint->value)[0] &= 0x3f;
-        break;
+        gquic_big_endian_transfer(val, buf, 1);
+        ((u_int8_t *) val)[0] &= 0x3f;
+        return 1;
 
     case 0x40:
-        varint->length = 2;
-        if (varint->length > size) {
+        if (2 > size) {
             return -3;
         }
-        gquic_big_endian_transfer(&varint->value, buf, 2);
-        ((unsigned char *) &varint->value)[1] &= 0x3f;
-        break;
+        gquic_big_endian_transfer(val, buf, 2);
+        ((unsigned char *) val)[1] &= 0x3f;
+        return 2;
 
     case 0x80:
-        varint->length = 4;
-        if (varint->length > size) {
+        if (4 > size) {
             return -3;
         }
-        gquic_big_endian_transfer(&varint->value, buf, 4);
-        ((unsigned char *) &varint->value)[3] &= 0x3f;
-        break;
+        gquic_big_endian_transfer(val, buf, 4);
+        ((unsigned char *) val)[3] &= 0x3f;
+        return 4;
 
     case 0xc0:
-        varint->length = 8;
-        if (varint->length > size) {
+        if (8 > size) {
             return -3;
         }
-        gquic_big_endian_transfer(&varint->value, buf, 8);
-        ((unsigned char *) &varint->value)[7] &= 0x3f;
-        break;
+        gquic_big_endian_transfer(val, buf, 8);
+        ((unsigned char *) val)[7] &= 0x3f;
+        return 8;
 
     default:
         return -4;
     }
-
-    return varint->length;
 }
 
