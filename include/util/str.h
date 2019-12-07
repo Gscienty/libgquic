1bdff8a663fd03e92f040e71bdfc49589c5421db
Gscienty <gaoxiaochuan@hotmail.com>
2019-12-07 12:26:22 -0500

    check param type

--- include/util/str.h
+++ include/util/str.h
@@ -11,31 +11,32 @@ struct gquic_str_s {
 
 #define GQUIC_STR_VAL(p) (\
                           ({\
-                           gquic_str_t *_ = NULL;\
-                           (void) (_ == (p));\
+                           gquic_str_t *__str = NULL;\
+                           (void) (__str == (p));\
                            }),\
                            (p) == NULL ? NULL : (((gquic_str_t *) (p))->val)\
-                          )
-#define GQUIC_STR_SIZE(p) (\
-                          ({\
-                           gquic_str_t *_ = NULL;\
-                           (void) (_ == (p));\
-                           }),\
-                          (p) == NULL ? 0 : (((gquic_str_t *) (p))->size)\
                            )
+#define GQUIC_STR_SIZE(p) (\
+                           ({\
+                            gquic_str_t *__str = NULL;\
+                            (void) (__str == (p));\
+                            }),\
+                            (p) == NULL ? 0 : (((gquic_str_t *) (p))->size)\
+                            )
+#define GQUIC_STR_FIRST_BYTE(p) (\
+                                 ({\
+                                  gquic_str_t *__str = NULL;\
+                                  (void) (__str == (p)); \
+                                  }),\
+                                  (p) == NULL ? 0 : *((u_int8_t *) GQUIC_STR_VAL((p))) \
+                                  )
 
 int gquic_str_init(gquic_str_t *str);
-
 int gquic_str_alloc(gquic_str_t *str, size_t size);
-
 int gquic_str_reset(gquic_str_t *str);
-
 int gquic_str_copy(gquic_str_t *str, const gquic_str_t *ref);
-
 int gquic_str_set(gquic_str_t *const str, const char *const val);
-
 int gquic_str_test_echo(const gquic_str_t *const str);
-
 int gquic_str_cmp(const gquic_str_t *const str_a, const gquic_str_t *const str_b);
 
 #endif
