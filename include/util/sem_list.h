fa700b572d1a6c40bc62e7c603acaba5bc646364
Gscienty <gaoxiaochuan@hotmail.com>
2019-12-07 12:25:51 -0500

    feat: add sem_list pop, wait_pop, push, rpush methods

--- include/util/sem_list.h
+++ include/util/sem_list.h
@@ -5,6 +5,11 @@
 #include <semaphore.h>
 
 #define GQUIC_SEM_LIST(h) (&((h)->list))
+#define GQUIC_SEM_LIST_FIRST(h) (GQUIC_LIST_FIRST(GQUIC_SEM_LIST((h))))
+#define GQUIC_SEM_LIST_LOCK(h) (sem_wait(&((h)->mtx)))
+#define GQUIC_SEM_LIST_UNLOCK(h) (sem_post(&((h)->mtx)))
+#define GQUIC_SEM_LIST_WAIT(h) (sem_wait(&((h)->sem)))
+#define GQUIC_SEM_LIST_NOTIFY(h) (sem_post(&((h)->sem)))
 
 typedef struct gquic_sem_list_s gquic_sem_list_t;
 struct gquic_sem_list_s {
@@ -15,5 +20,10 @@ struct gquic_sem_list_s {
 
 int gquic_sem_list_init(gquic_sem_list_t *const list);
 int gquic_sem_list_sem_release(gquic_sem_list_t *const list);
+
+int gquic_sem_list_pop(void **const event, gquic_sem_list_t *const list);
+int gquic_sem_list_waiting_pop(void **const event, gquic_sem_list_t *const list, int (*cmp)(const void *const, const void *const), const void *const arg);
+int gquic_sem_list_push(gquic_sem_list_t *const list, void *const event);
+int gquic_sem_list_rpush(gquic_sem_list_t *const list, void *const event);
 
 #endif
--- util/sem_list.c
+++ util/sem_list.c
@@ -19,3 +19,64 @@ int gquic_sem_list_sem_release(gquic_sem_list_t *const list) {
 
     return 0;
 }
+
+int gquic_sem_list_pop(void **const event, gquic_sem_list_t *const list) {
+    if (event == NULL || list == NULL) {
+        return -1;
+    }
+    GQUIC_SEM_LIST_WAIT(list);
+    GQUIC_SEM_LIST_LOCK(list);
+    if (gquic_list_head_empty(GQUIC_SEM_LIST(list))) {
+        GQUIC_SEM_LIST_UNLOCK(list);
+        return -2;
+    }
+    *event = GQUIC_SEM_LIST_FIRST(list);
+    gquic_list_remove(*event);
+    GQUIC_SEM_LIST_UNLOCK(list);
+    return 0;
+}
+
+int gquic_sem_list_waiting_pop(void **const event, gquic_sem_list_t *const list, int (*cmp)(const void *const, const void *const), const void *const arg) {
+    if (event == NULL || list == NULL || cmp == NULL) {
+        return -1;
+    }
+init:
+    GQUIC_SEM_LIST_WAIT(list);
+    GQUIC_SEM_LIST_LOCK(list);
+    if (gquic_list_head_empty(GQUIC_SEM_LIST(list))) {
+        GQUIC_SEM_LIST_UNLOCK(list);
+        return -2;
+    }
+    *event = GQUIC_SEM_LIST_FIRST(list);
+    if (cmp(event, arg) != 0) {
+        *event = NULL;
+        GQUIC_SEM_LIST_UNLOCK(list);
+        GQUIC_SEM_LIST_NOTIFY(list);
+        goto init;
+    }
+    gquic_list_remove(*event);
+    GQUIC_SEM_LIST_UNLOCK(list);
+    return 0;
+}
+
+int gquic_sem_list_push(gquic_sem_list_t *const list, void *const event) {
+    if (list == NULL || event == NULL) {
+        return -1;
+    }
+    GQUIC_SEM_LIST_LOCK(list);
+    gquic_list_insert_before(GQUIC_SEM_LIST(list), event);
+    GQUIC_SEM_LIST_UNLOCK(list);
+    GQUIC_SEM_LIST_NOTIFY(list);
+    return 0;
+}
+
+int gquic_sem_list_rpush(gquic_sem_list_t *const list, void *const event) {
+    if (list == NULL || event == NULL) {
+        return -1;
+    }
+    GQUIC_SEM_LIST_LOCK(list);
+    gquic_list_insert_after(GQUIC_SEM_LIST(list), event);
+    GQUIC_SEM_LIST_UNLOCK(list);
+    GQUIC_SEM_LIST_NOTIFY(list);
+    return 0;
+}
