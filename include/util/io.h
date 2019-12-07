73e8d65692535bf27792d4b3d67566b9963c81cd
Gscienty <gaoxiaochuan@hotmail.com>
2019-12-07 14:26:27 -0500

    feat: add macro (IO_READ IO_WRITE)

--- include/util/io.h
+++ include/util/io.h
@@ -7,8 +7,11 @@ typedef struct gquic_io_s gquic_io_t;
 struct gquic_io_s {
     void *self;
     int (*write) (size_t *const, void *const, const gquic_str_t *const);
-    int (*read) (size_t *const, void *const, const gquic_str_t *const);
+    int (*read) (size_t *const, gquic_str_t *const, void *const);
 };
+
+#define GQUIC_IO_WRITE(r, p, s) ((p)->write((r), (p)->self, (s)))
+#define GQUIC_IO_READ(r, s, p) ((p)->read((r), (s), (p)->self))
 
 int gquic_io_init(gquic_io_t *const output);
 
