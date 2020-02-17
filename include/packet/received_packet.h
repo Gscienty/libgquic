f7e6ba9e46bfd10622c9f6778080a50c872cac58
Gscienty <gaoxiaochuan@hotmail.com>
2020-02-17 05:25:06 -0500

    feat: add received_packet

---
+++ include/packet/received_packet.h
@@ -0,0 +1,20 @@
+#ifndef _LIBGQUIC_PACKET_RECEIVED_PACKET_H
+#define _LIBGQUIC_PACKET_RECEIVED_PACKET_H
+
+#include "net/addr.h"
+#include "util/str.h"
+#include "packet/packet_pool.h"
+#include <sys/types.h>
+
+typedef struct gquic_received_packet_s gquic_received_packet_t;
+struct gquic_received_packet_s {
+    gquic_net_addr_t remote_addr;
+    u_int64_t recv_time;
+    gquic_str_t data;
+
+    gquic_packet_buffer_t *buffer;
+};
+
+int gquic_received_packet_init(gquic_received_packet_t *const recv_packet);
+
+#endif
---
+++ packet/received_packet.c
@@ -0,0 +1,13 @@
+#include "packet/received_packet.h"
+
+int gquic_received_packet_init(gquic_received_packet_t *const recv_packet) {
+    if (recv_packet == NULL) {
+        return -1;
+    }
+    gquic_net_addr_init(&recv_packet->remote_addr);
+    recv_packet->recv_time = 0;
+    gquic_str_init(&recv_packet->data);
+    recv_packet->buffer = NULL;
+
+    return 0;
+}
