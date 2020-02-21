2129f12d6d4266c38cc36d0e4ba15a5df12e35b4
Gscienty <gaoxiaochuan@hotmail.com>
2020-02-21 18:26:00 -0500

    feat: add header param

--- include/packet/received_packet.h
+++ include/packet/received_packet.h
@@ -1,6 +1,7 @@
 #ifndef _LIBGQUIC_PACKET_RECEIVED_PACKET_H
 #define _LIBGQUIC_PACKET_RECEIVED_PACKET_H
 
+#include "packet/header.h"
 #include "net/addr.h"
 #include "util/str.h"
 #include "packet/packet_pool.h"
@@ -13,6 +14,9 @@ struct gquic_received_packet_s {
     gquic_str_t data;
 
     gquic_packet_buffer_t *buffer;
+
+    gquic_reader_str_t reader;
+    gquic_packet_header_t header;
 };
 
 int gquic_received_packet_init(gquic_received_packet_t *const recv_packet);
