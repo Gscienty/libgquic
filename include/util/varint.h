3589065aa2d6b3798fb957e1eaabfd2ff858198f
Gscienty <gaoxiaochuan@hotmail.com>
2020-02-25 20:20:58 -0500

    feat: try serialized_size

--- include/util/varint.h
+++ include/util/varint.h
@@ -8,4 +8,18 @@ ssize_t gquic_varint_size(const u_int64_t *const val);
 int gquic_varint_serialize(const u_int64_t *const val, gquic_writer_str_t *const writer);
 int gquic_varint_deserialize(u_int64_t *const val, gquic_reader_str_t *const reader);
 
+static inline size_t gquic_varint_serialized_size(const u_int8_t first_byte) {
+    switch (first_byte & 0xc0) {
+    case 0x00:
+        return 1;
+    case 0x40:
+        return 2;
+    case 0x80:
+        return 4;
+    case 0xc0:
+        return 8;
+    }
+    return 0;
+}
+
 #endif
