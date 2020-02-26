07679e7c3822f73ddab550fedbe40b0f5da25e06
Gscienty <gaoxiaochuan@hotmail.com>
2020-02-25 20:19:44 -0500

    feat: remove useless params

--- include/packet/received_packet.h
+++ include/packet/received_packet.h
@@ -12,13 +12,13 @@ struct gquic_received_packet_s {
     gquic_net_addr_t remote_addr;
     u_int64_t recv_time;
     gquic_str_t data;
+    gquic_str_t dst_conn_id;
 
     gquic_packet_buffer_t *buffer;
-
-    gquic_reader_str_t reader;
-    gquic_packet_header_t header;
 };
 
 int gquic_received_packet_init(gquic_received_packet_t *const recv_packet);
+gquic_received_packet_t *gquic_received_packet_copy(gquic_received_packet_t *const recv_packet);
+
 
 #endif
--- packet/received_packet.c
+++ packet/received_packet.c
@@ -1,4 +1,5 @@
 #include "packet/received_packet.h"
+#include <malloc.h>
 
 int gquic_received_packet_init(gquic_received_packet_t *const recv_packet) {
     if (recv_packet == NULL) {
@@ -8,7 +9,20 @@ int gquic_received_packet_init(gquic_received_packet_t *const recv_packet) {
     recv_packet->recv_time = 0;
     gquic_str_init(&recv_packet->data);
     recv_packet->buffer = NULL;
-    gquic_packet_header_init(&recv_packet->header);
+    gquic_str_init(&recv_packet->dst_conn_id);
+
+    return 0;
+}
+
+gquic_received_packet_t *gquic_received_packet_copy(gquic_received_packet_t *const recv_packet) {
+    gquic_received_packet_t *ret = NULL;
+    if (recv_packet == NULL) {
+        return NULL;
+    }
+    if ((ret = malloc(sizeof(gquic_received_packet_t))) == NULL) {
+        return NULL;
+    }
+    *ret = *recv_packet;
 
     return 0;
 }
