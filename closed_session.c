5b7951a3ca06e3bfbf85c858d8a797a1d3c6073c
Gscienty <gaoxiaochuan@hotmail.com>
2020-02-26 20:14:24 -0500

    feat: add closed_local_session thread

--- closed_session.c
+++ closed_session.c
@@ -1,11 +1,41 @@
 #include "closed_session.h"
+#include "util/sem_list.h"
 #include <malloc.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+typedef struct gquic_closed_local_session_s gquic_closed_local_session_t;
+struct gquic_closed_local_session_s {
+    gquic_net_conn_t *conn;
+    gquic_str_t data;
+    
+    sem_t close_mtx;
+    int close_flag;
+
+    gquic_sem_list_t run_event_list;
+    int counter;
+
+    int is_client;
+
+    pthread_t thread;
+};
+
+#define GQUIC_CLOSED_LOCAL_SESSION_EVENT_CLOSE 0x01
+#define GQUIC_CLOSED_LOCAL_SESSION_EVENT_RECEIVED_PACKET 0x02
 
 static int gquic_closed_remote_session_handle_packet(void *const, gquic_received_packet_t *const);
 static int gquic_closed_remote_session_close(void *const);
 static int gquic_closed_remote_session_destory(void *const, const int);
 static int gquic_closed_remote_session_client_is_client(void *const);
 static int gquic_closed_remote_session_server_is_client(void *const);
+
+static int gquic_closed_local_session_handle_packet(void *const, gquic_received_packet_t *const);
+static int gquic_closed_local_session_close(void *const);
+static int gquic_closed_local_session_destory(void *const, const int);
+static int gquic_closed_local_session_is_client(void *const);
+static int gquic_closed_local_session_dtor(gquic_closed_local_session_t *const);
+
+static void *gquic_closed_local_session_thread(void *const);
 
 gquic_packet_handler_t *gquic_closed_remote_session_client_alloc() {
     gquic_packet_handler_t *ret = malloc(sizeof(gquic_packet_handler_t));
@@ -68,5 +98,125 @@ static int gquic_closed_remote_session_client_is_client(void *const _) {
 }
 static int gquic_closed_remote_session_server_is_client(void *const _) {
     (void) _;
+    return 0;
+}
+
+gquic_packet_handler_t *gquic_closed_local_session_alloc(gquic_net_conn_t *const conn, gquic_str_t *const conn_close_packet, const int is_client) {
+    gquic_closed_local_session_t *sess = malloc(sizeof(gquic_closed_local_session_t));
+    gquic_packet_handler_t *ret = malloc(sizeof(gquic_packet_handler_t));
+    if (sess == NULL || ret == NULL) {
+        return NULL;
+    }
+    sess->conn = conn;
+    sess->close_flag = 0;
+    sess->data = *conn_close_packet;
+    sem_init(&sess->close_mtx, 0, 1);
+    sess->counter = 0;
+    sess->is_client = is_client;
+    gquic_sem_list_init(&sess->run_event_list);
+
+    ret->closer.closer.cb = gquic_closed_local_session_close;
+    ret->closer.closer.self = sess;
+    ret->destroy.cb = gquic_closed_local_session_destory;
+    ret->destroy.self = sess;
+    ret->handle_packet.cb = gquic_closed_local_session_handle_packet;
+    ret->handle_packet.self = sess;
+    ret->is_client.cb = gquic_closed_local_session_is_client;
+    ret->is_client.self = sess;
+
+    pthread_create(&sess->thread, NULL, gquic_closed_local_session_thread, sess);
+
+    return ret;
+}
+
+static int gquic_closed_local_session_is_client(void *const sess_) {
+    gquic_closed_local_session_t *const sess = sess_;
+    if (sess == NULL) {
+        return -1;
+    }
+    return sess->is_client;
+}
+
+static int gquic_closed_local_session_close(void *const sess_) {
+    return gquic_closed_local_session_destory(sess_, 0);
+}
+
+static int gquic_closed_local_session_destory(void *const sess_, const int _) {
+    (void) _;
+    gquic_closed_local_session_t *const sess = sess_;
+    if (sess == NULL) {
+        return -1;
+    }
+    sem_wait(&sess->close_mtx);
+    if (sess->close_flag) {
+        sem_post(&sess->close_mtx);
+        return 0;
+    }
+    sess->close_flag = 1;
+    u_int8_t *event = NULL;
+    if ((event = gquic_list_alloc(sizeof(u_int8_t)))== NULL) {
+        sem_post(&sess->close_mtx);
+        return -2;
+    }
+    *event = GQUIC_CLOSED_LOCAL_SESSION_EVENT_CLOSE;
+    sem_post(&sess->close_mtx);
+
+    gquic_sem_list_push(&sess->run_event_list, event);
+    return 0;
+}
+
+static int gquic_closed_local_session_handle_packet(void *const sess_, gquic_received_packet_t *const rp) {
+    gquic_closed_local_session_t *const sess = sess_;
+    if (sess_  == NULL || rp == NULL) {
+        return -1;
+    }
+    u_int8_t *event = NULL;
+    if ((event = gquic_list_alloc(sizeof(u_int8_t)))== NULL) {
+        return -2;
+    }
+    *event = GQUIC_CLOSED_LOCAL_SESSION_EVENT_RECEIVED_PACKET;
+    gquic_packet_buffer_put(rp->buffer);
+    free(rp);
+    gquic_sem_list_push(&sess->run_event_list, event);
+    return 0;
+}
+
+static void *gquic_closed_local_session_thread(void *const sess_) {
+    u_int8_t *event = NULL;
+    int i = 0;
+    gquic_closed_local_session_t *const sess = sess_;
+    if (sess  == NULL) {
+        return NULL;
+    }
+
+    for ( ;; ) {
+loop_start:
+        gquic_sem_list_pop((void **) &event, &sess->run_event_list);
+        switch (*event) {
+        case GQUIC_CLOSED_LOCAL_SESSION_EVENT_CLOSE:
+            gquic_list_release(event);
+            gquic_closed_local_session_dtor(sess);
+            return NULL;
+        case GQUIC_CLOSED_LOCAL_SESSION_EVENT_RECEIVED_PACKET:
+            sess->counter++;
+            for (i = sess->counter; i > 1; i = i / 2) {
+                if (i % 2 != 0) {
+                    goto loop_start;
+                }
+            }
+            gquic_net_conn_write(sess->conn, &sess->data);
+        }
+    }
+
+    return NULL;
+}
+
+static int gquic_closed_local_session_dtor(gquic_closed_local_session_t *const sess) {
+    if (sess == NULL) {
+        return -1;
+    }
+    gquic_str_reset(&sess->data);
+    // TODO clear sem_list
+
     return 0;
 }
