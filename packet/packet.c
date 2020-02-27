7239c867c7fe5dc775e4881dc5fe00f393e3a925
Gscienty <gaoxiaochuan@hotmail.com>
2020-02-27 11:32:12 -0500

    feat: impl dtor

--- packet/packet.c
+++ packet/packet.c
@@ -1,5 +1,6 @@
 #include "packet/packet.h"
 #include "frame/meta.h"
+#include <malloc.h>
 
 int gquic_packet_init(gquic_packet_t *const packet) {
     if (packet == NULL) {
@@ -19,6 +20,13 @@ int gquic_packet_init(gquic_packet_t *const packet) {
 int gquic_packet_dtor(gquic_packet_t *const packet) {
     if (packet == NULL) {
         return -1;
+    }
+    if (packet->frames != NULL) {
+        while (!gquic_list_head_empty(packet->frames)) {
+            gquic_frame_release(*(void **) GQUIC_LIST_FIRST(packet->frames));
+            gquic_list_release(GQUIC_LIST_FIRST(packet->frames));
+        }
+        free(packet->frames);
     }
 
     return 0;
