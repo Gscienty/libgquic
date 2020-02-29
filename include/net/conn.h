36c4130c9cc28ecf27f01a5f1c7bf563cfad6560
Gscienty <gaoxiaochuan@hotmail.com>
2020-02-29 05:11:20 -0500

    feat add overwrite conn write method (for debug)

--- include/net/conn.h
+++ include/net/conn.h
@@ -8,8 +8,16 @@ typedef struct gquic_net_conn_s gquic_net_conn_t;
 struct gquic_net_conn_s {
     gquic_net_addr_t addr;
     int fd;
+
+    struct {
+        void *self;
+        int (*cb) (void *const, const gquic_str_t *const);
+    } write;
 };
 
+#define GQUIC_NET_CONN_WRITE(writer, raw) ((writer)->write.cb((writer)->write.self, (raw)))
+
+int gquic_net_conn_init(gquic_net_conn_t *const conn);
 int gquic_net_conn_write(gquic_net_conn_t *const conn, const gquic_str_t *const raw);
 
 #endif
--- net/conn.c
+++ net/conn.c
@@ -1,10 +1,30 @@
 #include "net/conn.h"
 #include <unistd.h>
 
+int gquic_net_conn_init(gquic_net_conn_t *const conn) {
+    if (conn == NULL) {
+        return -1;
+    }
+    gquic_net_addr_init(&conn->addr);
+    conn->fd = -1;
+    conn->write.cb = NULL;
+    conn->write.self = NULL;
+
+    return 0;
+}
+
 int gquic_net_conn_write(gquic_net_conn_t *const conn, const gquic_str_t *const raw) {
     if (conn == NULL || raw == NULL) {
         return -1;
     }
-    write(conn->fd, GQUIC_STR_VAL(raw), GQUIC_STR_SIZE(raw));
-    return 0;
+    if (conn->write.self != NULL) {
+        return GQUIC_NET_CONN_WRITE(conn, raw);
+    }
+
+    if (conn->addr.type == AF_INET) {
+        return sendto(conn->fd, GQUIC_STR_VAL(raw), GQUIC_STR_SIZE(raw), 0, (struct sockaddr *) &conn->addr.addr.v4, sizeof(struct sockaddr_in));
+    }
+    else {
+        return sendto(conn->fd, GQUIC_STR_VAL(raw), GQUIC_STR_SIZE(raw), 0, (struct sockaddr *) &conn->addr.addr.v6, sizeof(struct sockaddr_in6));
+    }
 }
