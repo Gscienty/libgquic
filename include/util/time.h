0d216317587c679355d490e0aad6dec500ecd3b3
Gscienty <gaoxiaochuan@hotmail.com>
2020-03-14 07:11:08 -0400

    refactor: add gquic_time_t type

--- include/util/time.h
+++ include/util/time.h
@@ -5,8 +5,10 @@
 #include <sys/time.h>
 #include <stddef.h>
 
+typedef u_int64_t gquic_time_t;
+
 int gquic_time_since_milli(int64_t *ret, const struct timeval *const t);
-static inline u_int64_t gquic_time_now() {
+static inline gquic_time_t gquic_time_now() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return tv.tv_sec * 1000 * 1000 + tv.tv_usec;
