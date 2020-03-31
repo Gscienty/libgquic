05409b3465baeaa343c7242c36a20e3db4b50e50
Gscienty <gaoxiaochuan@hotmail.com>
2020-03-31 13:24:39 -0400

    test: rbtree find and multi insert

--- test/util/rbtree.c
+++ test/util/rbtree.c
@@ -74,3 +74,56 @@ GQUIC_UNIT_TEST(rbtree_insert_conflict) {
 
     GQUIC_PROCESS_DONE(GQUIC_SUCCESS);
 }
+
+GQUIC_UNIT_TEST(rbtree_insert_more) {
+    gquic_rbtree_t *rbt = NULL;
+
+    GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_root_init(&rbt) == GQUIC_SUCCESS);
+    GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_is_nil(rbt));
+
+    int i;
+    for (i = 0; i < 10; i++) {
+        gquic_rbtree_t *node = NULL;
+        GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_alloc(&node, sizeof(u_int8_t), sizeof(u_int8_t)) == GQUIC_SUCCESS);
+        *(u_int8_t *) GQUIC_RBTREE_KEY(node) = i;
+        GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_insert(&rbt, node) == GQUIC_SUCCESS);
+    }
+    GQUIC_UNIT_TEST_EXPECT(!gquic_rbtree_is_nil(rbt));
+
+    gquic_rbtree_t *eachor = NULL;
+    i = 0;
+    GQUIC_RBTREE_EACHOR_BEGIN(eachor, rbt);
+    i++;
+    GQUIC_RBTREE_EACHOR_END(eachor);
+    GQUIC_UNIT_TEST_EXPECT(i == 10);
+
+    GQUIC_PROCESS_DONE(GQUIC_SUCCESS);
+}
+
+GQUIC_UNIT_TEST(rbtree_insert_find) {
+    gquic_rbtree_t *rbt = NULL;
+
+    GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_root_init(&rbt) == GQUIC_SUCCESS);
+    GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_is_nil(rbt));
+
+    int i;
+    for (i = 0; i < 10; i++) {
+        gquic_rbtree_t *node = NULL;
+        GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_alloc(&node, sizeof(u_int8_t), sizeof(u_int8_t)) == GQUIC_SUCCESS);
+        *(u_int8_t *) GQUIC_RBTREE_KEY(node) = i;
+        GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_insert(&rbt, node) == GQUIC_SUCCESS);
+    }
+    GQUIC_UNIT_TEST_EXPECT(!gquic_rbtree_is_nil(rbt));
+
+    for (i = 5; i < 15; i++) {
+        const gquic_rbtree_t *node = NULL;
+        if (i < 10) {
+            GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_find(&node, rbt, (void *) &i, sizeof(u_int8_t)) == GQUIC_SUCCESS);
+        }
+        else {
+            GQUIC_UNIT_TEST_EXPECT(gquic_rbtree_find(&node, rbt, (void *) &i, sizeof(u_int8_t)) == GQUIC_EXCEPTION_NOT_FOUND);
+        }
+    }
+
+    GQUIC_PROCESS_DONE(GQUIC_SUCCESS);
+}
