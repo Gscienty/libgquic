4556c58f1f51fcf39762c4dbf317002f934eb596
Gscienty <gaoxiaochuan@hotmail.com>
2020-04-01 05:35:49 -0400

    refactor: use macos not_implement

--- include/streams/stream_sender.h
+++ include/streams/stream_sender.h
@@ -2,6 +2,7 @@
 #define _LIBGQUIC_STREAMS_STREAM_SENDER_H
 
 #include <sys/types.h>
+#include "exception.h"
 
 typedef struct gquic_stream_sender_s gquic_stream_sender_t;
 struct gquic_stream_sender_s {
@@ -22,15 +23,15 @@ int gquic_stream_sender_init(gquic_stream_sender_t *const sender);
 
 #define GQUIC_SENDER_QUEUE_CTRL_FRAME(sender, frame) \
     (((sender)->queue_ctrl_frame.self) == NULL \
-     ? -1 \
+     ? GQUIC_EXCEPTION_NOT_IMPLEMENTED \
      : ((sender)->queue_ctrl_frame.cb((sender)->queue_ctrl_frame.self, (frame))))
 #define GQUIC_SENDER_ON_HAS_STREAM_DATA(sender, sid) \
     (((sender)->on_has_stream_data.self) == NULL \
-    ? -1 \
+    ? GQUIC_EXCEPTION_NOT_IMPLEMENTED \
     : ((sender)->on_has_stream_data.cb((sender)->on_has_stream_data.self, (sid))))
 #define GQUIC_SENDER_ON_STREAM_COMPLETED(sender, sid) \
     (((sender)->on_stream_completed.self) == NULL \
-    ? -1 \
+    ? GQUIC_EXCEPTION_NOT_IMPLEMENTED \
     : ((sender)->on_stream_completed.cb((sender)->on_stream_completed.self, (sid))))
 
 typedef struct gquic_uni_stream_sender_s gquic_uni_stream_sender_t;
