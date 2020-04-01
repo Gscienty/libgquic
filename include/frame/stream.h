a40f6961352c8cc68f3ba3d096b46808165ebbd2
Gscienty <gaoxiaochuan@hotmail.com>
2020-04-01 05:35:10 -0400

    feat: add stream set/get fin bit tools

--- include/frame/stream.h
+++ include/frame/stream.h
@@ -1,8 +1,10 @@
 #ifndef _LIBGQUIC_FRAME_STREAM_H
 #define _LIBGQUIC_FRAME_STREAM_H
 
+#include "frame/meta.h"
 #include "streams/type.h"
 #include "util/str.h"
+#include "exception.h"
 
 typedef struct gquic_frame_stream_s gquic_frame_stream_t;
 struct gquic_frame_stream_s {
@@ -14,5 +16,22 @@ struct gquic_frame_stream_s {
 gquic_frame_stream_t *gquic_frame_stream_alloc();
 u_int64_t gquic_frame_stream_data_capacity(const u_int64_t size, const gquic_frame_stream_t *const frame);
 int gquic_frame_stream_split(gquic_frame_stream_t **new_frame, gquic_frame_stream_t *const frame, const u_int64_t size);
+
+static inline int gquic_frame_stream_set_fin(gquic_frame_stream_t *const frame) {
+    if (frame == NULL) {
+        GQUIC_PROCESS_DONE(GQUIC_EXCEPTION_PARAMETER_UNEXCEPTED);
+    }
+    GQUIC_FRAME_META(frame).type |= 0x01;
+
+    GQUIC_PROCESS_DONE(GQUIC_SUCCESS);
+}
+
+static inline int gquic_frame_stream_get_fin(gquic_frame_stream_t *const frame) {
+    if (frame == NULL) {
+        return 1;
+    }
+
+    return (GQUIC_FRAME_META(frame).type & 0x01) != 0x00;
+}
 
 #endif
