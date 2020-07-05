898abbc0f84b40094e9330b751177be70b5af99f
Gscienty <gaoxiaochuan@hotmail.com>
2020-07-05 04:52:18 -0400

    hybrid_slow_start add comment

--- include/cong/hybrid_slow_start.h
+++ include/cong/hybrid_slow_start.h
@@ -1,23 +1,63 @@
+/* include/cong/hybrid_slow_start.h 慢启动模块定义
+ *
+ * Copyright (c) 2019-2020 Gscienty <gaoxiaochuan@hotmail.com>
+ *
+ * Distributed under the MIT software license, see the accompanying
+ * file LICENSE or https://www.opensource.org/licenses/mit-license.php .
+ */
+
 #ifndef _LIBGQUIC_CONG_HYBIRD_SLOW_START_H
 #define _LIBGQUIC_CONG_HYBIRD_SLOW_START_H
 
 #include <sys/types.h>
+#include <stdbool.h>
+#include "exception.h"
 
+/**
+ * 慢启动实现模块
+ */
 typedef struct gquic_cong_hybrid_slow_start_s gquic_cong_bybrid_slow_start_t;
 struct gquic_cong_hybrid_slow_start_s {
+
+    // 开始慢启动阶段时的packet号
     u_int64_t end_pn;
+
+    // 记录最后发送的packet号, 当慢启动开始时，将end_pn置为该值
     u_int64_t last_sent_pn;
-    int started;
+
+    // 当前是否处于慢启动阶段
+    bool started;
+
+    // 慢启动阶段最小RTT时间
     u_int64_t current_min_rtt;
+
+    // RTT样本数量
     u_int32_t rtt_sample_count;
-    int hystart_found;
+
+    // 慢启动阶段是否已探知该阶段的窗口大小
+    bool hystart_found;
 };
 
-int gquic_cong_hybrid_slow_start_init(gquic_cong_bybrid_slow_start_t *const slowstart);
-int gquic_hybrid_slow_start_start_recv_round(gquic_cong_bybrid_slow_start_t *const slowstart, const u_int64_t last_sent);
-int gquic_hybrid_slow_start_should_exit(gquic_cong_bybrid_slow_start_t *const slowstart,
-                                        const u_int64_t last_rtt,
-                                        const u_int64_t min_rtt,
-                                        const u_int64_t cwnd);
+/**
+ * 慢启动模块初始化
+ *
+ * @param slowstart: slowstart
+ * 
+ * @return exception
+ */
+gquic_exception_t gquic_cong_hybrid_slow_start_init(gquic_cong_bybrid_slow_start_t *const slowstart);
+
+/**
+ * 判断当前状态下是否应退出慢启动阶段
+ *
+ * @param slowstart: slowstart
+ * @param last_rtt: 最新的RTT取样
+ * @param min_rtt: 最小的RTT取样
+ * @param cwnd: 当前拥塞窗口
+ *
+ * @return 是否应退出慢启动阶段
+ */
+bool gquic_hybrid_slow_start_should_exit(gquic_cong_bybrid_slow_start_t *const slowstart,
+                                         const u_int64_t last_rtt, const u_int64_t min_rtt, const u_int64_t cwnd);
 
 #endif
--- src/cong/hybrid_slow_start.c
+++ src/cong/hybrid_slow_start.c
@@ -2,43 +2,53 @@
 #include "exception.h"
 #include <stddef.h>
 
+/**
+ * 开始慢启动阶段
+ * 
+ * @param slowstart: slowstart
+ * @param last_sent: 开始慢启动阶段时发送的packet号
+ *
+ * @return exception
+ */
+static gquic_exception_t gquic_hybrid_slow_start_start_recv_round(gquic_cong_bybrid_slow_start_t *const slowstart, const u_int64_t last_sent);
+
 gquic_exception_t gquic_cong_hybrid_slow_start_init(gquic_cong_bybrid_slow_start_t *const slowstart) {
     if (slowstart == NULL) {
         GQUIC_PROCESS_DONE(GQUIC_EXCEPTION_PARAMETER_UNEXCEPTED);
     }
     slowstart->current_min_rtt = 0;
     slowstart->end_pn = 0;
-    slowstart->hystart_found = 0;
+    slowstart->hystart_found = false;
     slowstart->last_sent_pn = 0;
     slowstart->rtt_sample_count = 0;
-    slowstart->started = 0;
+    slowstart->started = false;
 
     GQUIC_PROCESS_DONE(GQUIC_SUCCESS);
 }
 
-int gquic_hybrid_slow_start_start_recv_round(gquic_cong_bybrid_slow_start_t *const slowstart, const u_int64_t last_sent) {
+static gquic_exception_t gquic_hybrid_slow_start_start_recv_round(gquic_cong_bybrid_slow_start_t *const slowstart, const u_int64_t last_sent) {
     if (slowstart == NULL) {
         GQUIC_PROCESS_DONE(GQUIC_EXCEPTION_PARAMETER_UNEXCEPTED);
     }
     slowstart->end_pn = last_sent;
     slowstart->current_min_rtt = 0;
     slowstart->rtt_sample_count = 0;
-    slowstart->started = 1;
+    slowstart->started = true;
 
     GQUIC_PROCESS_DONE(GQUIC_SUCCESS);
 }
 
-int gquic_hybrid_slow_start_should_exit(gquic_cong_bybrid_slow_start_t *const slowstart,
+bool gquic_hybrid_slow_start_should_exit(gquic_cong_bybrid_slow_start_t *const slowstart,
                                         const u_int64_t last_rtt, const u_int64_t min_rtt, const u_int64_t cwnd) {
     u_int64_t min_rtt_increase_threshold = 0;
     if (slowstart == NULL) {
-        return 0;
+        return false;
     }
     if (!slowstart->started) {
         gquic_hybrid_slow_start_start_recv_round(slowstart, slowstart->last_sent_pn);
     }
     if (slowstart->hystart_found) {
-        return 1;
+        return true;
     }
     slowstart->rtt_sample_count++;
     if (slowstart->rtt_sample_count <= 8) {
@@ -52,7 +62,7 @@ int gquic_hybrid_slow_start_should_exit(gquic_cong_bybrid_slow_start_t *const sl
         min_rtt_increase_threshold = min_rtt_increase_threshold < 4000 ? 4000 : min_rtt_increase_threshold;
 
         if (slowstart->current_min_rtt > (min_rtt + min_rtt_increase_threshold)) {
-            slowstart->hystart_found = 1;
+            slowstart->hystart_found = true;
         }
     }
     return cwnd >= 16 && slowstart->hystart_found;
